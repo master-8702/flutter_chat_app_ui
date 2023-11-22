b0e1d32c75157d71b27233459e6b8fecb7d48316
Ibrahim Selman <ibroselman@gmail.com>
2023-11-22 22:30:20 +0300

    Fix message time alignment

--- lib/widgets/chat_mesage.dart
+++ lib/widgets/chat_mesage.dart
@@ -40,6 +40,7 @@ class _ChatMessageState extends State<ChatMessage> {
                 ),
 
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Row(
                 children: [
